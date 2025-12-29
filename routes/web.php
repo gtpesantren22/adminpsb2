4d86d6fd3d00a6be5df71089031962ec78776123
gtpesantren22 <cakromat26@gmail.com>
2025-12-29 20:32:01 +0700

    update route login

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
     return view('main.login');
-});
+})->name('login');
 
 Route::middleware('auth')->group(function () {
 
